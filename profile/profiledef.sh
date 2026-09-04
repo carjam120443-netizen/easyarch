4885ac6ff5df4dc17a24686a147bc81d25de87f1
carjam120443-netizen <carjam120443@gmail.com>
2026-09-04 13:20:11 -0400

    Add safe XZ ISO compression

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -12,6 +12,12 @@ bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 pacman_conf="pacman.conf"
 arch="x86_64"
 
+# Keep the live root filesystem compact. XZ with the x86 BCJ filter is the
+# same general compression approach used by Arch's releng profile: smaller
+# images at the cost of some extra compression time, with no runtime penalty.
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+
 declare -A file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
