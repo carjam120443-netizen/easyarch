42cbe6e48ad1957067708caaa50a1952e9ebf3c8
carjam120443-netizen <carjam120443@gmail.com>
2026-09-04 12:59:28 -0400

    Fix archiso file permissions array

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
 
-iso_name="easyarch"
-iso_label="EASYARCH_$(date +%Y%m)"
+aiso_name="easyarch"
+iso_label="EASYARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="EasyArch"
 iso_application="EasyArch Live/Install Media"
 iso_version="0.1"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 
-file_permissions=(
+declare -A file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
