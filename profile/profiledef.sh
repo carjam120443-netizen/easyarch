4bfd22b539d54d65f83d43a0d12d83ae17a71b54
carjam120443-netizen <carjam120443@gmail.com>
2026-09-04 13:00:06 -0400

    Fix missing archiso pacman config

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-aiso_name="easyarch"
+iso_name="easyarch"
 iso_label="EASYARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="EasyArch"
 iso_application="EasyArch Live/Install Media"
@@ -9,6 +9,7 @@ iso_version="0.1"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux' 'uefi.systemd-boot')
+pacman_conf="pacman.conf"
 arch="x86_64"
 
 declare -A file_permissions=(
